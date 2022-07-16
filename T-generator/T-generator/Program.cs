e96d18563603d4537e53e5c2a101312e984852bf
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-07-16 16:35:42 +0100

    added checks on the input
    the input value (T) is now checked for being a number in a 9x9 table.
    if T is found to be less than 1 or more than 90 then an ArgumentException is thrown
    
    modified: T-generator/T-generator/Program.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- T-generator/T-generator/Program.cs
+++ T-generator/T-generator/Program.cs
@@ -6,6 +6,15 @@ namespace lightman210567.TGenerator
     {
         public static int TGenerate(int T) // requires a int value returned
         {
+            if (T < 1)
+            {
+                throw new ArgumentException($"Input number must be 1 or larger");
+            } 
+            else if (T > 90)
+            {
+                throw new ArgumentException("Input number must be less than or equal to 90");
+            }
+
             int n = T; // sets the value n to the value of T. n is used for the term to term rule of T-totals
 
             int TTotal = (5 * n) - 63; // multiplies n by 5 then subtracts 63 to get the T-total
