f5b274b7858b9289afac2cc92271e771028bd285
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2022-07-16 18:27:50 +0100

    added a unit test
    added a unit test to check that the calculation returns the correct value, (when T = 50, then 187 should be returned)
    
    new file: T-generator/T-generator.Tests/CalculationTests.cs
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

---
+++ T-generator/T-generator.Tests/CalculationTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lightman210567.TGenerator;
+
+namespace TGenerator.Tests
+{
+    [TestClass]
+    public class CalculationTests
+    {
+        [TestMethod]
+        [DataRow(50)]
+        public void ReturnsCorrectValue_Is187(int T)
+        {
+            int result = Generator.TGenerate(T);
+
+            Assert.AreEqual(187, result);
+        }
+    }
+}
